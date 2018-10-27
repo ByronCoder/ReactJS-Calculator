35f81c805b03d58639b751580bc2c112e2375c4f
ByronCoder <ByronCoder>
2018-10-27 14:23:34 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,147 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state  = {
+      previousValue: '',
+      currentValue: '0',
+      formula: '0'     
+      
+    }
+    
+    this.numberHandler = this.numberHandler.bind(this);
+    this.clearHandler = this.clearHandler.bind(this);
+    this.operatorHandler = this.operatorHandler.bind(this);
+    this.decimalHandler = this.decimalHandler.bind(this);
+    this.equalsHandler = this.equalsHandler.bind(this);
+    this.replaceLast = this.replaceLast.bind(this);
+  }
+  
+ replaceLast(find, replace, string) {
+    var lastIndex = string.lastIndexOf(find);
+    
+    if (lastIndex === -1) {
+        return string;
+    }
+    
+    var beginString = string.substring(0, lastIndex);
+    var endString = string.substring(lastIndex + find.length);
+    
+    return beginString + replace + endString;
+}
+
+  
+  clearHandler(e) {
+    this.setState({
+      currentValue: '0',
+      previousValue: '',
+      formula: '0'
+    })
+  }
+  numberHandler(e) {
+    this.setState({
+      currentValue: this.state.currentValue += e.target.value,
+      previousValue: e.target.value,
+      formula: this.state.formula == '0' ? e.target.value : this.state.formula += e.target.value
+    })
+   
+ }
+
+    
+  operatorHandler(e) {
+    
+  
+      if(!this.state.previousValue.match(/[+|\-|*|\/]/)) {
+      this.setState({
+       currentValue: '0',
+      formula: this.state.formula += e.target.value,
+        previousValue: e.target.value
+    })
+    
+    
+  }
+  else{
+   
+    this.setState({
+       previousValue: e.target.value,
+       formula: this.replaceLast(this.state.previousValue, e.target.value, this.state.formula)
+    })
+   
+  }
+  }
+  
+    
+ decimalHandler(e) {
+   
+   if(!this.state.currentValue.includes('.'))
+    {
+     this.setState({
+      currentValue: e.target.value,
+      formula: this.state.formula += e.target.value })
+      
+    }
+   
+ }
+ 
+  equalsHandler() {
+    const formula = this.state.formula;
+    const result = eval(formula);
+    
+    this.setState({
+      formula: result
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div id="main"><div className="calculator">
+        <Display previousValue={this.state.previousValue}
+                 currentValue={this.state.currentValue}
+          formula={this.state.formula}/>
+        
+        <Buttons 
+                 clearHandler={this.clearHandler}
+                 numberHandler={this.numberHandler} 
+                 operatorHandler={this.operatorHandler}
+                 decimalHandler={this.decimalHandler}
+                 equalsHandler={this.equalsHandler}/>
+        </div>
+      </div>);
   }
 }
 
-export default App;
+class Buttons extends React.Component {
+  render() {
+    return(
+      <div className="grid-container">
+        <button id="clear" className="grid-item clear" value="AC" onClick={this.props.clearHandler}>AC</button>
+        <button id="divide" className="grid-item operatorStyle" onClick={this.props.operatorHandler} value="/">/</button>
+        <button id="multiply" className="grid-item operatorStyle" onClick={this.props.operatorHandler} value="*" >X</button>
+        <button id="seven" className="grid-item numberStyle" onClick={this.props.numberHandler} value="7">7</button>
+        <button id="eight" className="grid-item numberStyle" onClick={this.props.numberHandler} value="8">8</button>
+        <button id="nine" className="grid-item numberStyle" onClick={this.props.numberHandler} value="9">9</button>
+        <button id="subtract" className="grid-item operatorStyle" onClick={this.props.operatorHandler} value="-">-</button>
+        <button id="four" className="grid-item numberStyle" onClick={this.props.numberHandler} value="4">4</button>
+        <button id="five" className="grid-item numberStyle" onClick={this.props.numberHandler} value="5">5</button>
+        <button id="six" className="grid-item numberStyle" onClick={this.props.numberHandler} value="6">6</button>
+        <button id="add" className="grid-item operatorStyle" onClick={this.props.operatorHandler} value="+">+</button>
+        <button id="one" className="grid-item numberStyle" onClick={this.props.numberHandler} value="1">1</button>
+        <button id="two" className="grid-item numberStyle" onClick={this.props.numberHandler} value="2">2</button>
+        <button id="three" className="grid-item numberStyle" onClick={this.props.numberHandler} value="3">3</button>
+        <button id="equals" className="grid-item equals" onClick={this.props.equalsHandler} value="=">=</button>
+        <button id="zero" className="grid-item numberStyle zero" onClick={this.props.numberHandler} value="0">0</button>
+        <button id="decimal" className="grid-item numberStyle" onClick={this.props.decimalHandler} value=".">.</button>
+      </div>
+    
+    )
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    return (<div id="display">{this.props.formula}</div>);
+  }
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/css/index.css';
+import './styles/css/App.css';
+import Calculator from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
